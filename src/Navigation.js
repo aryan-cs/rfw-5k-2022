069e579a8b96d42f97df78a8edf74545d1ef0799
aryan-cs <aryan05g@gmail.com>
2022-08-04 00:11:29 -0500

    fixed responsitivity

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,16 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import styles from './Navigation.module.css';
 
-function Navigation(){
+function Navigation () {
     return ( 
             <nav className={styles.navBar}>
                 <div>
                     <div className={styles.menu}>
                         <ul className>
-                            <li className={styles.navBarEntry}><Link to = '/' className={styles.link}>About Us</Link></li>
-                            <li className={styles.navBarEntry}><Link to = '/' className={styles.link}>Register</Link></li>
-                            <li className={styles.navBarEntry}><Link to = '/' className={styles.link}>Donate</Link></li>
-                            <li className={styles.navBarEntry}><Link to = '/' className={styles.link}>Learn More</Link></li>
+                            <li className={styles.navBarEntry}><Link to = '/about-us' className={styles.link}>About Us</Link></li>
+                            <li className={styles.navBarEntry}><Link to = '/registration' className={styles.link}>Register</Link></li>
                         </ul>
                     </div>
                     <button className={styles.menuButton}>
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,1 +1,5 @@
- 
+function Registration () {
+    
+}
+
+export default Registration;
