874d928a6a9f084aa6c605b4f6c2e2bbb062f0d0
SidD11111 <79270579+SidD11111@users.noreply.github.com>
2022-08-27 14:37:43 -0500

    Add files via upload

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -10,38 +10,63 @@ function Partners () {
 
             <div className = "Partners">
 
-                <img src = {process.env.PUBLIC_URL + "/assets/partners/palatinepack.png" }
-                     onClick = {() => window.open("https://palatinepack.com/", '_blank', 'noopener, noreferrer')}
-                     className = 'Partner-link'
-                     alt = "Palatine Pack"
-                     rel = "noreferrer"
-                     target = "_blank"
-                     href = "https://palatinepack.com/"/>  
-                <img src = {process.env.PUBLIC_URL + "/assets/partners/waterproject.png" }
-                     onClick = {() => window.open("https://thewaterproject.org/", '_blank', 'noopener, noreferrer')}
-                     className = 'Partner-link'
-                     alt = "The Water Project" 
-                     rel = "noreferrer" 
-                     target = "_blank" 
-                     href = "https://thewaterproject.org/"/>  
+                <div className = 'sponsor-section'>
+                    <img src = {process.env.PUBLIC_URL + "/assets/partners/palatinepack.png" }
+                        onClick = {() => window.open("https://palatinepack.com/", '_blank', 'noopener, noreferrer')}
+                        className = 'Partner-link'
+                        alt = "Palatine Pack"
+                        rel = "noreferrer"
+                        target = "_blank"
+                        href = "https://palatinepack.com/"/>  
 
-                <img src = {process.env.PUBLIC_URL + "/assets/partners/superiorcapital.png"}
-                     onClick = {() => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=Rahul@SuperiorCapital1.com", '_blank', 'noopener, noreferrer')}
-                     className = 'Partner-link' 
-                     alt = "Superior Capital"
-                     rel = "noreferrer" 
-                     target = "_blank" 
-                     href = "https://mail.google.com/mail/?view=cm&fs=1&to=Rahul@SuperiorCapital1.com"/>  
+                        <p className='sponsor-name'>Palatine Pack</p>
+                </div>
+                <div className = 'sponsor-section'>
+                    <img src = {process.env.PUBLIC_URL + "/assets/partners/waterproject.png" }
+                        onClick = {() => window.open("https://thewaterproject.org/", '_blank', 'noopener, noreferrer')}
+                        className = 'Partner-link'
+                        alt = "The Water Project" 
+                        rel = "noreferrer" 
+                        target = "_blank" 
+                        href = "https://thewaterproject.org/"/>  
+                        
+                        <p className='sponsor-name'>The Water Project</p>
+                </div>
 
-                {/* <img src = {process.env.PUBLIC_URL + "/assets/partners/rrpharmacy.png" }
-                     onClick = {() => window.open("http://ranareaganpharmacy.com/", '_blank', 'noopener, noreferrer')} 
-                     className = 'Partner-link' 
-                     alt = "Rana-Reagan Pharmacy" 
-                     rel = "noreferrer" 
-                     target = "_blank" 
-                     href = "http://ranareaganpharmacy.com/"/> */}
+                <div className = 'sponsor-section'>
+                    <img src = {process.env.PUBLIC_URL + "/assets/partners/superiorcapital.png"}
+                        onClick = {() => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=Rahul@SuperiorCapital1.com", '_blank', 'noopener, noreferrer')}
+                        className = 'Partner-link' 
+                        alt = "Superior Capital"
+                        rel = "noreferrer" 
+                        target = "_blank" 
+                        href = "https://mail.google.com/mail/?view=cm&fs=1&to=Rahul@SuperiorCapital1.com"/>  
+                        
+                        <p className='sponsor-name'>Superior Capital</p>
+                </div>
 
-                {/* add mara pharmicy and addison pharmacy */}
+                <div className = 'sponsor-section'>
+                    <img src = {process.env.PUBLIC_URL + "/assets/partners/addison.png"}
+                        onClick = {() => window.open("https://www.addisonrx.com/", '_blank', 'noopener, noreferrer')}
+                        className = 'Partner-link' 
+                        alt = "Addison Pharmacy"
+                        rel = "noreferrer" 
+                        target = "_blank" 
+                        href = "https://www.addisonrx.com/"/>  
+                        
+                        <p className='sponsor-name'>Addison Pharmacy</p>
+                </div>
+
+                <div className = 'sponsor-section'>
+                    <img src = {process.env.PUBLIC_URL + "/assets/partners/mara.png"}
+                        className = 'Partner-link' 
+                        alt = "Mara Pharmacy"
+                        rel = "noreferrer" 
+                        target = "_blank"/>  
+                        
+                        <p className='sponsor-name'>Mara Pharmacy</p>
+                </div>
+
 
             </div>
 
